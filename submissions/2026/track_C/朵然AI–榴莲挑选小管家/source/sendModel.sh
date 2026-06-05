de512c1feb95e314ad8995abaf26021929f91a33
chensiqi <chensiqi2007@gmail.com>
2026-06-05 21:51:40 +0800

    feat: 提交 Project-Resurgence 项目至 track_D 赛道

--- submissions/2026/track_C/朵然AI–榴莲挑选小管家/source/sendModel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-adb shell "mkdir -p /sdcard/Android/data/com.winter.durianai/files/models"
-adb push ./gemma-4-e2b-it.litertlm /sdcard/Android/data/com.winter.durianai/files/models/gemma-4-e2b-it.litertlm
